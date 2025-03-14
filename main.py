6269d4a9d8360bbbeadd9146fb09e853191cbdb3
Alhuvier <al.huvier@live.fr>
2025-03-14 22:56:52 +0100

    show number words in file

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 def main():
-    path_to_file = "./books/frankenstein.txt"
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        print(file_contents)
+    bookPath = "books/frankenstein.txt"
+    text = getBookText(bookPath)
+    num_words = getNumWords(text)
+    print(f"{num_words} words found in the document")
+
+
+def getBookText(path):
+    with open(path) as f:
+        return f.read()
+
+
+def getNumWords(text):
+    words = text.split()
+    return len(words)
 main()
